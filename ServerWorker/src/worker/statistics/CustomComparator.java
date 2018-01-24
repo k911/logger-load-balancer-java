951a1e9641d9ae74d2af20968b93956c005fc5af
AScislawska <aleksandra.scislawska@gmail.com>
2018-01-24 02:29:53 +0100

    statistics added

---
+++ ServerWorker/src/worker/statistics/CustomComparator.java
@@ -0,0 +1,10 @@
+package worker.statistics;
+
+import java.util.Comparator;
+
+public class CustomComparator implements Comparator<Long> {
+    @Override
+    public int compare(Long o1, Long o2) {
+        return 0;
+    }
+}
