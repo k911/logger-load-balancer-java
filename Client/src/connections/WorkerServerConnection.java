70976b9dc450e4ea930fa8fbaef1c8da23a44c41
FieryCod <kwcharllie379@gmail.com>
2018-01-24 16:11:24 +0100

    Add todos, create a job from a GUI

--- Client/src/connections/WorkerServerConnection.java
+++ Client/src/connections/WorkerServerConnection.java
@@ -2,15 +2,23 @@ package connections;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
+
+import worker.communication.JobRequest;
+import worker.communication.job.Job;
 
 public class WorkerServerConnection {
     private final static Logger logger = Logger.getLogger(WorkerServerConnection.class.getName());
 	private Socket socket;
+	private JobRequest jobRequest;
 	
-	public WorkerServerConnection (String hostname, int port) {
+	public WorkerServerConnection (String hostname, int port, Map<Long, Job> jobs) {
 		try {
-			this.socket = new Socket("localhost", port);
+			socket = new Socket("localhost", port);
+			jobRequest = new JobRequest("Author " + (new Date().getTime()), jobs);
 		} catch (IOException err) {
 			logConnectionIssue(err);
 		}
--- Client/src/controller/LaunchTaskController.java
+++ Client/src/controller/LaunchTaskController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import application.Main;
 import connections.SchedulerConnection;
 import connections.WorkerServerConnection;
@@ -13,10 +16,13 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
+import worker.communication.job.Job;
+import worker.communication.job.JobType;
 import javafx.scene.control.RadioButton;
 
 public class LaunchTaskController {
-	private static int currTaskChosen = 1;
+	private static Long currTaskChosen = (long) 1;
+	private Map<Long, Job> jobs = new HashMap<>();
 
 	@FXML
 	AnchorPane launchTask;
@@ -69,12 +75,30 @@ public class LaunchTaskController {
 
 	@FXML
 	public void launchTask() {
+		// Create job then
+		Job job = createJobWithType();
+
+		// Put the job to Map
+		putJob((long) 0, job);
+
+		// Get available worker
 		Worker worker = (Worker) Main.getSchedulerConnection().getWorker();
-	    WorkerServerConnection workerServerConnection = new WorkerServerConnection(worker.getHost(), worker.getPort());
+
+		// Establish connection
+		WorkerServerConnection workerServerConnection = new WorkerServerConnection(worker.getHost(), worker.getPort(),
+				jobs);
+
+		// Send request
+		// @TODO: SEND REQUEST HERE
+		// workerSeverConnection.sendRequest();
+		
+		// Get results and show them
+		// @TODO: FUNCTION FOR GETTING RESULTS	
+		taskOutputArea.setText("RESULT HERE");
 	}
 
 	public static void setCurrTaskChosen(ObservableValue<? extends Toggle> toggleObservableValue) {
-		currTaskChosen = (Integer) toggleObservableValue.getValue().getUserData();
+		currTaskChosen = (Long) toggleObservableValue.getValue().getUserData();
 	}
 
 	private void initializeUserData() {
@@ -94,6 +118,10 @@ public class LaunchTaskController {
 		t9.setUserData(9);
 	}
 
+	private void putJob(Long key, Job job) {
+		jobs.put(key, job);
+	}
+
 	private void initializeEventListeners() {
 		taskGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 			public void changed(ObservableValue<? extends Toggle> toggleObservableValue, Toggle toggle,
@@ -103,6 +131,42 @@ public class LaunchTaskController {
 				changeTextTaskNumberLabel();
 			}
 		});
+	}
+
+	private Job createJobWithType() {
+		JobType jobType = null;
+
+		switch (currTaskChosen.intValue()) {
+		case 1:
+			jobType = JobType.CALC_ARITHMETIC_MEAN;
+			break;
+		case 2:
+			jobType = JobType.CALC_GEOMETRIC_MEAN;
+			break;
+		case 3:
+			jobType = JobType.CALC_MEDIAN;
+			break;
+		case 4:
+			jobType = JobType.CALC_NUMBER_OCCURENCES;
+			break;
+		case 5:
+			jobType = JobType.CALC_PHRASE_OCCURENCES;
+			break;
+		case 6:
+			jobType = JobType.CALC_STANDARD_DEVIATION;
+			break;
+		case 7:
+			jobType = JobType.CALC_VARIANCE;
+			break;
+		case 8:
+			jobType = JobType.FIND_MAX;
+			break;
+		case 9:
+			jobType = JobType.FIND_MIN;
+			break;
+		}
+
+		return new Job(jobType, null);
 	}
 
 	private void changeTextTaskNumberLabel() {
